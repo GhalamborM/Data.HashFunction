9a06676da036b6d192a5d31a962a581aecf560e4
LordOf Xen <38406718+LordOfXen@users.noreply.github.com>
2024-11-03 02:47:09 +0300

    Testings for Blake3
    Add Test project for Blake3

---
+++ src/OpenSource.Data.HashFunction.Test/Blake3/Blake3Factory_Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using Xunit;
+using OpenSource.Data.HashFunction.Blake3;
+
+namespace OpenSource.Data.HashFunction.Test.Blake3
+{
+    public class Blake3Factory_Tests
+    {
+        [Fact]
+        public void Blake3Factory_Instance_IsDefined()
+        {
+            Assert.NotNull(Blake3Factory.Instance);
+            Assert.IsType<Blake3Factory>(Blake3Factory.Instance);
+        }
+        
+        [Fact]
+        public void blake3Factory_Create_Config_IsNull_Throws()
+        {
+            var blake3Factory = Blake3Factory.Instance;
+
+            Assert.Equal(
+                "config",
+                Assert.Throws<ArgumentNullException>(
+                        () => blake3Factory.Create(null))
+                    .ParamName);
+        }
+
+        [Fact]
+        public void blake3Factory_Create_Config_Works()
+        {
+			var blake3Config = new Blake3Config();
+
+			var blake3Factory = Blake3Factory.Instance;
+			var blake3 = blake3Factory.Create(blake3Config);
+
+            Assert.NotNull(blake3);
+            Assert.IsType<Blake3_Implementation>(blake3);
+
+            var resultingBlake3Config = blake3.Config;
+
+            Assert.Equal(resultingBlake3Config.HashSizeInBits, resultingBlake3Config.HashSizeInBits);
+            Assert.Equal(resultingBlake3Config, resultingBlake3Config);
+		}
+    }
+}
